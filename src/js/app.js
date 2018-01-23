b5f47f999399741b70bc57fdda1db0b1d2f7cb7b
Yegorich75 <yegorich75@gmail.com>
2018-01-23 18:36:21 +0200

    PIXI Setup

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import PIXI from 'pixi.js';
 
-console.log(PIXI);
-let app = new PIXI.Application(300,300,{backgroundColor: 0x000000});
+let app = new PIXI.Application({width: 300, height: 300, backgroundColor: 0x000000});
 
 document.body.appendChild(app.view);
